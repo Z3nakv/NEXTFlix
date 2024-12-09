67cacc9d2c039e135ae11bf81e50d2a6b9a8cfee
Adrian <ad.rivarola18@gmail.com>
2024-12-09 10:30:39 -0500

    imports

--- components/mediaInfo/SelectSeasons.tsx
+++ components/mediaInfo/SelectSeasons.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { LazyRow } from './LazyRow';
+import { LazyRow } from '@/components/LazyRow';
 import { movieProps, seasonsProps } from '@/types';
 
 type SeasonSelectType = {
@@ -10,17 +10,16 @@ type SeasonSelectType = {
 };
 
 export default function SeasonSelect({ result, mediaId, mediaType }: SeasonSelectType) {
-    // Filtrar temporadas válidas, excluyendo "Specials"
+
     const seasons : seasonsProps[] = result?.seasons;
     
     const validSeasons = seasons?.filter((season) => season.name.toLowerCase() !== 'specials') || [];
     
-    // Estado para la temporada seleccionada, comenzando con la primera temporada válida
     const [selectedSeason, setSelectedSeason] = useState(validSeasons[0]?.season_number || 1);
 
     const handleSeasonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const season = parseInt(event.target.value, 10); // Convertir el valor a número
-        setSelectedSeason(season); // Actualizar el estado de la temporada seleccionada
+        const season = parseInt(event.target.value, 10);
+        setSelectedSeason(season);
     };
 
     return (
@@ -29,8 +28,8 @@ export default function SeasonSelect({ result, mediaId, mediaType }: SeasonSelec
                 name="seasons"
                 id="selectSeason"
                 className="selectOptions"
-                value={selectedSeason} // Sincronizar el valor del select con el estado
-                onChange={handleSeasonChange} // Manejar el cambio de temporada
+                value={selectedSeason}
+                onChange={handleSeasonChange}
             >
                 {validSeasons.map((season) => (
                     <option value={season.season_number} key={season.id}>
@@ -41,7 +40,6 @@ export default function SeasonSelect({ result, mediaId, mediaType }: SeasonSelec
 
             {mediaType === 'tv' ? (
                 <div className="md:ml-[-50px]">
-                    {/* LazyRow carga dinámicamente la temporada seleccionada */}
                     <LazyRow
                         key={selectedSeason}
                         title={''}
